91c20bce5051777c9d2955bf3781b267ec2c8080
Mark Story <mark@mark-story.com>
2008-10-04 16:43:32 -0400

    Updating eclipse code completion to include more CakePHP core helpers

--- eclipse/code-completion-in-views.php
+++ eclipse/code-completion-in-views.php
@@ -2,19 +2,17 @@
 exit();
 //Add in some helpers so the code assist works much better.
 if(false) {
-   $ajax = new AjaxHelper();
-   $cache = new CacheHelper();
-   $form = new FormHelper();
-   $html = new HtmlHelper();
-   $javascript = new JavascriptHelper();
-   $number = new NumberHelper();
-   $session = new SessionHelper();
-   $text = new TextHelper();
-   $time = new TimeHelper();
-   $forms = new FormsHelper();
-   $pagination = new PaginationHelper();
-   $access = new AccessHelper();
-   $tree = new TreeHelper();
-   $xls = new xlsHelper();
+	$ajax = new AjaxHelper();
+	$form = new FormHelper();
+	$html = new HtmlHelper();
+	$javascript = new JavascriptHelper();
+	$number = new NumberHelper();
+	$session = new SessionHelper();
+	$text = new TextHelper();
+	$time = new TimeHelper();
+	$pagination = new PaginationHelper();
+	$rss = new RssHelper();
+	$xml = new XmlHelper();
+	$number = new NumberHelper();
 }
 ?>
