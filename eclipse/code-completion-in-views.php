e7079b1cdba0a0e0541ce651963385e648ac28d5
Mark Story <mark@mark-story.com>
2008-10-04 15:47:09 -0400

    Adding eclipse templates and helper files.

---
+++ eclipse/code-completion-in-views.php
@@ -0,0 +1,20 @@
+<?php
+exit();
+//Add in some helpers so the code assist works much better.
+if(false) {
+   $ajax = new AjaxHelper();
+   $cache = new CacheHelper();
+   $form = new FormHelper();
+   $html = new HtmlHelper();
+   $javascript = new JavascriptHelper();
+   $number = new NumberHelper();
+   $session = new SessionHelper();
+   $text = new TextHelper();
+   $time = new TimeHelper();
+   $forms = new FormsHelper();
+   $pagination = new PaginationHelper();
+   $access = new AccessHelper();
+   $tree = new TreeHelper();
+   $xls = new xlsHelper();
+}
+?>
